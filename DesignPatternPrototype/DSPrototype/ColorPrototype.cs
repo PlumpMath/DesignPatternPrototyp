b71a5db6aabb7a838b76018cc85af1c2467a9df1
Plitzko <daniel.plitzko@gmail.com>
2016-01-13 10:03:10 +0100

    Init

---
+++ DesignPatternPrototype/DSPrototype/ColorPrototype.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSPrototype
+{
+    /// <summary>
+    /// The 'Prototype' abstract class
+    /// </summary>
+    abstract class ColorPrototype
+    {
+        public abstract ColorPrototype Clone();
+    }
+}
